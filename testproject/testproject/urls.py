79880484c40e0bd4df061806a8a364165b73f20f
shnyoo <shnyoo@ucdavis.edu>
2023-01-25 21:09:33 -0800

    Google map implementation

--- testproject/testproject/urls.py
+++ testproject/testproject/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-import home.views
+from home import views
 from accounts import views as accounts_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home.views.home, name="메인"),
+    path('', views.home, name="home"),
+    path('map/', views.map, name="map"),
 
     path('login/', accounts_views.login, name='login'),
     path('logout/', accounts_views.logout, name='logout')
