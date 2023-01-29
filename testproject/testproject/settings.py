fe36dc42f004a369cb186bfc20556ee9986f3fdf
Jin Ho Yon <jyon@ucdavis.edu>
2023-01-28 21:16:33 -0800

    userdetail
    1) testproject/accounts/templates/userdetail.html
    
    2) testproject/static/css/userdetail.css

--- testproject/testproject/settings.py
+++ testproject/testproject/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'testproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mysql',
-        'USER': 'root',
-        'PASSWORD': 'justdohit',
-        'HOST': '127.0.0.1',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
